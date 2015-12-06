47bfee236723287910c0d78c591c1c890b6b186c
codethejason <tigergoldfish17@gmail.com>
2015-12-06 18:32:36 -0500

    Fixed bio and added cascading effect

---
+++ js/hackucteam.js
@@ -0,0 +1,8 @@
+/*Cascading Effect*/
+$(document).ready(function(){
+    $('.card').each(function(i){
+        setTimeout(function(){
+            $('.card').eq(i).addClass('is-visible');
+        }, 125*i);
+    });
+});
