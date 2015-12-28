3c4c43b5ac62f87a4e0cfc5a458331aa68dc0a99
Annie <anniezhou301@gmail.com>
2015-12-28 01:37:26 -0500

    fix

--- js/hackuc.js
+++ js/hackuc.js
@@ -105,23 +105,6 @@ $("#faq .card-header").on("click", function() {
     }
 });
 
-//twitter widget
-<script>window.twttr = (function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0],
-    t = window.twttr || {};
-  if (d.getElementById(id)) return t;
-  js = d.createElement(s);
-  js.id = id;
-  js.src = "https://platform.twitter.com/widgets.js";
-  fjs.parentNode.insertBefore(js, fjs);
- 
-  t._e = [];
-  t.ready = function(f) {
-    t._e.push(f);
-  };
- 
-  return t;
-}(document, "script", "twitter-wjs"));</script>
 
 /*!
  * Bootstrap v3.3.4 (http://getbootstrap.com)
